813c7499274eccf324bc794098bcfc1d6c8f93ea
Zofikar <pawel.chwalczyk@gmail.com>
2023-08-06 22:10:34 +0200

    updated to use decimals

--- PST Packer/Program.cs
+++ PST Packer/Program.cs
@@ -1,167 +1,43 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using PST_Packer;
+﻿using PST_Packer;
+using MoreLinq;
 
 class Program
-{
-    // bits to pack default is last 5
-    static int BitsToPack = 5;
-    // remember to add arrays in good spot to All[]
-    static int PieceTypesCount = 6;
-    // to get negative values make negative offset as packer doesnt check sign
-    // its default for 5 bits packed this lets us achieve result in range -16 to 15
-    static int offset = -16;
-    // if you need to multiply your values by static factor after adding offset use this
-    // most likely your square values will be 0, 10, 20 ,25, 50, maybe some like -40 but hey you can achieve it by static multipier to save space in memory which will allow you to use less tokens
-    static int multiplier = 5;
-    // if you want different datatype make sure to update other functions
-    static List<ulong> Packed = new();
-
-
-    // Not important
-    static Stopwatch stopWatch = new Stopwatch();
-    static int calls = 0;
-
-    static uint[][][] All = new uint[2][][] { new uint[6][] { PST.PawnEarly, PST.KnightEarly, PST.BishopEarly, PST.RookEarly, PST.QueenEarly, PST.KingEarly }, new uint[6][] { PST.PawnLate, PST.KnightLate, PST.BishopLate, PST.RookLate, PST.QueenLate, PST.KingLate } };
-
+{ 
+    static int[] pesto = PST.mg_pawn_table.Concat(PST.eg_pawn_table).Concat(PST.mg_knight_table).Concat(PST.eg_knight_table).Concat(PST.mg_bishop_table).Concat(PST.eg_bishop_table).Concat(PST.mg_rook_table).Concat(PST.eg_rook_table)
+        .Concat(PST.mg_queen_table).Concat(PST.eg_queen_table).Concat(PST.mg_king_table).Concat(PST.eg_king_table).ToArray();
     static int Main()
     {
-        var result = Test().ToString();
-        if (result != true.ToString())
+        decimal[] packedPesto = PackPesto(pesto);
+        int[] unpackedPesto = Unpack(packedPesto);
+        pesto[128] = -167;
+        pesto[149] = 129;
+        for(int i = 0; i < unpackedPesto.Length; i++)
         {
-            Console.WriteLine($"Error:\n{result}");
-            return 1;
+            if (unpackedPesto[i] != pesto[i]) Console.WriteLine(string.Format("{0} != {1}",unpackedPesto[i], pesto[i]));
         }
-        Console.WriteLine("Everything went good");
-        Console.WriteLine("Test was passed");
-        string path = Path.Combine(Directory.GetCurrentDirectory(), "PST.cs");
-        using StreamReader reader = new(path);
-        string txt = reader.ReadToEnd();
-        string offsetString = (offset != 0 ? (offset > 0 ? $" + {offset}" : $" - {-offset}") : "");
-        string returnString = (multiplier != 0 && multiplier != 1 ? $"return ( (Early{offsetString})*{multiplier}, (Late{offsetString})*{multiplier} )" : $"return ( Early{offsetString}, Late{offsetString} )");
-        string output = $@"
-static ulong[] Packed = {{
-        {string.Join(",\n\t", Packed)}
-}};
-static (int, int) GetValue(int pieceType, int squareIndex)
-{{
-    int Early = 0, Late = 0;
-    for (int bitIndex = 0; bitIndex < {BitsToPack}; bitIndex++)
-    {{
-        var index = bitIndex * 2 + pieceType * ({BitsToPack} * 2);
-        Early = Early << 1 | (int)((Packed[index] >> SquareIndex) & 1);
-        Late = Late << 1 | (int)((Packed[index+1] >> SquareIndex) & 1);
-    }}
-    {returnString};
-}}
-";
-        Console.WriteLine(output);
-        Console.WriteLine();
-        Console.WriteLine("Copy this into your code and you should be good to go!");
-        Console.WriteLine($"It contains {Packed.Count} values this results in {TokenCounter.CountTokens(output).totalCount} instead of approx {TokenCounter.CountTokens(txt).totalCount}");
-        Console.WriteLine($"Your limit is {BitsToPack}bits per value!");
-        Console.WriteLine($"Average time to unpack single value for you is {new decimal(stopWatch.ElapsedTicks)/ calls / Stopwatch.Frequency * 1000}ms");
-        Console.WriteLine($"Time to unpack all values for you is {new decimal(stopWatch.ElapsedTicks) / Stopwatch.Frequency * 1000}ms");
+        Console.WriteLine(string.Join("m,\n", packedPesto)+'m');
         return 0;
     }
 
-    static void Pack()
+    static decimal[] PackPesto(int[] intArray)
     {
-        Packed.Clear();
-        for (int pieceType = 0; pieceType < PieceTypesCount; pieceType++)
+        intArray[128] = 0;
+        intArray[149] = 0;
+        return intArray.Select((x, i) => (byte)Convert.ToSByte(x - (i < 128 ? 64 : 0))).Batch(4).Select( x => BitConverter.ToInt32(x.ToArray())).Batch(3).Select(x =>
         {
-            for (int bit = BitsToPack - 1; bit >= 0; bit--)
-            {
-                for (int time = 0; time < 2; time++)
-                {
-                    ulong current = 0;
-                    for (int squareIndex = 63; squareIndex >= 0; squareIndex--)
-                    {
-                        current = current << 1 | GetBit(All[time][pieceType][squareIndex], bit);
-                    }
-                    Packed.Add(current);
-                }
-            }
-        }
-    }
-
-    static (int, int) GetValueWrapper(int pieceType, int squareIndex)
-    {
-        calls++;
-        stopWatch.Start();
-        var result = GetValue(pieceType, squareIndex);
-        stopWatch.Stop();
-        return result;
-    }
-
-    static (int, int) GetValue(int pieceType, int SquareIndex)
-    {
-        int Early = 0, Late = 0;
-        for (int bitIndex = 0; bitIndex < BitsToPack; bitIndex++)
-        {
-            var index = bitIndex * 2 + pieceType * (BitsToPack * 2);
-            Early = Early << 1 | (int)((Packed[index] >> SquareIndex) & 1);
-            Late = Late << 1 | (int)((Packed[index+1] >> SquareIndex) & 1);
-        }
-        return (Early, Late);
+            var t = x.ToList();
+            t.Add(0xF0000);
+            return new decimal(t.ToArray());
+        }).ToArray();
     }
 
 
-    static ulong GetBit(uint number, int index)
+
+    static int[] Unpack(decimal[] pestoPacked)
     {
-        int mask = 1 << index;
-        return (number & mask) != 0 ? 1UL : 0UL;
-    }
-
-    private static dynamic Test(bool makeRandom = false)
-    {
-        if (makeRandom)
-        {
-            Random rng = new Random();
-            for (int time = 0; time < 2; time++)
-            {
-                for (int pieceType = 0; pieceType < PieceTypesCount; pieceType++)
-                {
-                    for (int squareIndex = 0; squareIndex < 64; squareIndex++)
-                    {
-                        All[time][pieceType][squareIndex] = (uint)rng.Next(7);
-                    }
-                }
-            }
-        }
-        Pack();
-        for (int pieceType = 0; pieceType < PieceTypesCount; pieceType++)
-        {
-            for (int squareIndex = 0; squareIndex < 64; squareIndex++)
-            {
-                (int, int) returned = GetValueWrapper(pieceType, squareIndex);
-                (int, int) correct = ((int)All[0][pieceType][squareIndex], (int)All[1][pieceType][squareIndex]);
-                
-
-                if (correct != returned)
-                {
-                    return new Failed(pieceType, squareIndex, returned, correct);
-                }
-            }
-        }
-        return true;
-    }
-
-    public readonly struct Failed
-    {
-        public Failed(int pieceType, int squareIndex, (int, int) returnedValue, (int, int) correctValue)
-        {
-            this.pieceType = pieceType;
-            this.squareIndex = squareIndex;
-            this.returnedValue = returnedValue;
-            this.correctValue = correctValue;
-        }
-        public readonly int pieceType;
-        public readonly int squareIndex;
-        public readonly (int, int) returnedValue;
-        public readonly (int, int) correctValue;
-
-        public override readonly string ToString() => $"Piece Type: {pieceType}\nSquare Index: {squareIndex}\nReturned Value: {returnedValue}\nCorrect Value: {correctValue}";
+        int[] unpackedPesto = pestoPacked.SelectMany(x => decimal.GetBits(x).Take(3)).SelectMany(BitConverter.GetBytes).Select((x, i) => (sbyte)x + (i < 128 ? 64 : 0)).ToArray();
+        unpackedPesto[128] = -167;
+        unpackedPesto[149] = 129;
+        return unpackedPesto;
     }
 }
--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Simple program to pack PST tables into array of ulong
+# Simple program to pack PST tables into array of decimals
 ## Created to help to pack data for [Chess Challenge](https://github.com/SebLague/Chess-Challenge)
-
-### How to use?
-To use adjust multipier, offset, bits to pack in Program.cs and PST values in PST.cs
-
-### It doesn't support negative values, to achieve them use offset and scale all values to be greater or equal to 0 !
-For sake of simplicity just set your lowest value as offset and add to all values in PST.
-
-
-### What about tokens usage?
-It uses about 100 tokens depending on your offset and multipier + however many ulongs(or different data type if you change it) is needed to pack your data with your settings.
-For example I use 2 stages * 6 piece types and I get 5 bits of it with offset -16 and multiplier of 5 my result is 161 tokens.
-It will display exact amount of tokens as it uses [token counter from chess challange](https://github.com/SebLague/Chess-Challenge/tree/main/Chess-Challenge/src/Framework/Application/Helpers/Token%20Counter)
-
-### How does it work?
-It takes last bits of uints and adds them one by one into ulong untill its full then it adds ulong to list creates new ulong and repeats untill all values are inside ulong.
-In Main() you can see call to Test() with default value of false which means that your values will be packed then unpacked and compared to raw data to ensure that everything is correct, to unpack it uses same function as it gives you.
-
-### How to get my values?
-Pass pieceType and squareIndex as ints into GetValue function it gives you. 
-==**Watch out pieceType starts with 0 as a pawn in this function so make sure to either modify this function or decrement piece type value.**==
-It will return touple representing early and late stage(at least in my implementation) values which you can use to lerp between them based on state of the game or simply just pick one that fits you more at the exact time.
-It is possible to modify code to get only single stage if thats what you want it will save you about half of tokens over 100
-
-
-## If you see way to optimize, speed up or reduce token count I would apprieciete if you would share it
+Edit values in PST.cs to match your needs then copy output and unpack function into your code please note now its set to match default values from [chessprogramming.org](https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function) so adjust if your values exceed sbyte range
