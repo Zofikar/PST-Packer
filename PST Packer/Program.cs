fb07cd14209b71584eb4dde48978c8682ed4eea4
Zofikar <pawel.chwalczyk@gmail.com>
2023-08-06 22:21:53 +0200

    updated output

--- PST Packer/Program.cs
+++ PST Packer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using PST_Packer;
 using MoreLinq;
+using Microsoft.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 { 
@@ -8,6 +10,7 @@ class Program
     static int Main()
     {
         decimal[] packedPesto = PackPesto(pesto);
+        string unpackexample = "    static int[] Unpack(decimal[] pestoPacked)\r\n    {\r\n        int[] unpackedPesto = pestoPacked.SelectMany(x => decimal.GetBits(x).Take(3)).SelectMany(BitConverter.GetBytes).Select((x, i) => (sbyte)x + (i < 128 ? 64 : 0)).ToArray();\r\n        unpackedPesto[128] = -167;\r\n        unpackedPesto[149] = 129;\r\n        return unpackedPesto;\r\n    }";
         int[] unpackedPesto = Unpack(packedPesto);
         pesto[128] = -167;
         pesto[149] = 129;
@@ -15,7 +18,12 @@ class Program
         {
             if (unpackedPesto[i] != pesto[i]) Console.WriteLine(string.Format("{0} != {1}",unpackedPesto[i], pesto[i]));
         }
-        Console.WriteLine(string.Join("m,\n", packedPesto)+'m');
+        string output = $"decimal[] packed = [ {string.Join("m,", packedPesto) + 'm'}]";
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "PST.cs");
+        using StreamReader reader = new(path);
+        string txt = reader.ReadToEnd();
+        Console.WriteLine($"Token usage for array is {TokenCounter.CountTokens(output).totalCount} + unpack function about {TokenCounter.CountTokens(unpackexample).totalCount} compared to {TokenCounter.CountTokens(txt).totalCount}");
+        Console.WriteLine(output);
         return 0;
     }
 
